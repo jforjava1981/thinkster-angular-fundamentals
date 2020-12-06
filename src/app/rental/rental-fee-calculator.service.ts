3a4f9e1573edf294c5d9820ab2818d5b98b0f9e4
Shailesh Vaishampayan <jforjava1981@gmail.com>
2020-12-06 22:54:08 +0530

    Chapter 2 -Exercise 1-solution : creating service using CLI

---
+++ src/app/rental/rental-fee-calculator.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalFeeCalculatorService {
+
+  constructor() { }
+
+  calculate(): number {
+    return 2.99;
+  }
+}
