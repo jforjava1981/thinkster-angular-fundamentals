bf529bfbd2c3bc7d44750b714b294f9281feadf7
Shailesh Vaishampayan <jforjava1981@gmail.com>
2020-12-06 22:55:10 +0530

    Chapter-2 Exercise-2 create components using CLI & manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
+import { FlopVotesComponent } from './flop-votes/flop-votes.component';
+import { RentalCartComponent } from './rental-cart/rental-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent    
+    AppComponent,
+    MovieDisplayComponent,
+    FlopVotesComponent,
+    RentalCartComponent    
   ],
   imports: [
     BrowserModule
---
+++ src/app/rental-cart/rental-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-rental-cart',
+    templateUrl: './rental-cart.component.html',
+    styleUrls:['./rental-cart.component.css']
+})
+export class RentalCartComponent {
+    
+    constructor(){}
+}
