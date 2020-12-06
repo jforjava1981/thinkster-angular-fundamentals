9899a9520f7f8c5f8da0145d53017ab3e5c13ea9
Shailesh Vaishampayan <jforjava1981@gmail.com>
2020-12-06 16:04:52 +0530

    Exercise-1-Initial solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Movie } from './movie';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flopbster';
+  movies:Array<Movie> = [{
+    url : "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY67_CR0,0,45,67_AL_.jpg",
+    title:"Shawshank's redemption" 
+   },{
+     url : "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY67_CR1,0,45,67_AL_.jpg",
+     title:"The Godfather" 
+    },{
+     url : "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY67_CR0,0,45,67_AL_.jpg",
+     title:"The Dark Knight" 
+    },{
+     url : "https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX45_CR0,0,45,67_AL_.jpg",
+     title:"12 Angry Men" 
+    }];
+
+    voted:boolean = false;
+
+    constructor(){
+    }
+    
+    
+    onVote(movie) {
+      this.voted = true;
+      movie.isChosen = 'Chosen';
+    }  
+
 }
---
+++ src/app/movie.ts
@@ -0,0 +1,5 @@
+export interface Movie {
+    title:string,
+    url:string,
+    isChosen?:string
+}
